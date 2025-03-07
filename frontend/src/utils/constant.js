58d3e9b5c7b5785d1153bf98730a2eeebc68017b
hariom <nishaauradar@gmail.com>
2025-03-07 18:48:53 +0530

    url change

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const API_END_POINT = "http://localhost:8080/api/v1/user";
+export const API_END_POINT = "hhttps://netflix2-oqrt.onrender.com/api/v1/user";
 
  export const options = {
   method: 'GET',
